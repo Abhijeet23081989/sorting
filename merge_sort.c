fc1b457ed5fd77f02b72ece9127c9c69c1a3edd4
Abhijeet23081989 <abhijeet.srivastava2308@gmail.com>
2020-11-30 23:52:28 +0530

    Second Draft of merge sort

--- merge_sort.c
+++ merge_sort.c
@@ -4,7 +4,7 @@
 
 int* merge_sort(int*);
 int** split_array(int*);
-int* comp_merge(int**);
+int* comp_merge(int*,int*);
 
 int main()
 {
@@ -20,20 +20,10 @@ int main()
 	printf("Enter Nine Elements\n");
 	for(i=0;i<9;i++)
 		scanf("%d",arr+i);
-	merge_sort(arr);
+	split_array(arr);
 }
 
-int* merge_sort(int* arr)
-{
-	int count=sizeof(arr)/4;
-	int **arr_of_arr=NULL;
-	//1-> Split Array
-	arr_of_arr=split_array(arr);
-	//2-> Compare & merge
-	return comp_merge(arr_of_arr);
-}
-
-int** split_array(int* arr)
+int split_array(int* arr)
 {
 	int count=sizeof(arr)/4;
 	int mid=count/2;
@@ -42,25 +32,32 @@ int** split_array(int* arr)
 	{
 		arr_L=(int*)malloc(sizeof(int)*mid);
 		arr_R=(int*)malloc(sizeof(int)*(count-mid));
-	}
-	else
-		return NULL;
 	//---------------Actual Split Operation-----------------
 		/*Original Idea is to use loops<Safe idea>*/
 		for(int i=0;i<mid;i++)
 			*(arr_L+i)=*(arr+i);
-		for(int j=mid;j<count;j++)
-			*(arr_R+j)=*(arr+j);
+		for(int j=0;j<(count-mid);j++)
+		{
+			*(arr_R+j)=*(arr+i);
+			i++;
+		}
 		
 		/*My Idea is to point arr_R to right half of array<Eligent idea>*/
 
 		//arr_L=arr;
 		//arr_R=arr+mid;
 	//------------------------------------------------------
+	}
+	else
+		return 0;
 	split_array(arr_L);
 	split_array(arr_R);
+	comp_merge(arr_L,arr_R);
 }
 
-int* comp_merge(int** arr_of_arr)
+int comp_merge(int* arr_L,int* arr_R)
 {
+	//1.Compare Left and right array
+	for(int i=0;i<)
+	//2.Merge Left and Right Array in order accordingly
 }
