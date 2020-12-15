a204fae8d06d656e58c99de1fba0b4471450bf79
Abhijeet23081989 <abhijeet.srivastava2308@gmail.com>
2020-12-16 00:00:31 +0530

    15/12/2020-->Realized that after sorting and remerging the left and right array was not returning the newly merged array back to the call of split_array

--- merge_sort.c
+++ merge_sort.c
@@ -1,8 +1,10 @@
 //merge_sort algo
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
 
-int split_array(int*,int);
+int* split_array(int*,int);
 int comp_merge(int*,int*,int,int);
 int display_sorted_arr(int*,int);
 
@@ -20,8 +22,8 @@ int main()
 	//printf("Enter Nine Elements\n");
 	//for(i=0;i<9;i++)
 	//	scanf("%d",&arr[i]);
-	*(arr+0)=3;
-	*(arr+1)=9;
+	*(arr+0)=9;
+	*(arr+1)=3;
 	*(arr+2)=12;
 	*(arr+3)=1;
 	*(arr+4)=4;
@@ -30,20 +32,22 @@ int main()
 	*(arr+7)=8;
 	*(arr+8)=6;
 	display_sorted_arr(arr,sizeofarr);
-	split_array(arr,sizeofarr);
+	arr=split_array(arr,sizeofarr);
 }
 
-int split_array(int* arr,int sizeofarr)
+int* split_array(int* arr,int sizeofarr)
 {
 	int mid=sizeofarr/2;
 	int arr_Lsize=mid,arr_Rsize=sizeofarr-mid;
 	//printf("mid=%d\n",mid);
 	int i,j;
-	int* arr_L,*arr_R;
+	int* arr_L,*arr_R,*sorted_arr_L,*sorted_arr_R;
 	if(mid!=0)
 	{
 		arr_L=(int*)malloc(sizeof(int)*mid);
 		arr_R=(int*)malloc(sizeof(int)*(sizeofarr-mid));
+		memset(arr_L,0,mid);
+		memset(arr_R,0,sizeofarr-mid);
 	//---------------Actual Split Operation-----------------
 		/*Original Idea is to use loops<Safe idea>*/
 		for(i=0;i<arr_Lsize;i++)
@@ -74,37 +78,42 @@ int split_array(int* arr,int sizeofarr)
 	display_sorted_arr(arr_R,arr_Rsize);
 	printf("\n");
 	
-	split_array(arr_L,arr_Lsize);
-	split_array(arr_R,arr_Rsize);
+	sorted_arr_L=split_array(arr_L,arr_Lsize);//1.{3,9,12,1} , {3,9},{3},{12}
+	sorted_arr_R=split_array(arr_R,arr_Rsize);//1.{4,13,2,8,6},{9},{12,1},{1}
 	//comp_merge(arr_L,arr_R,arr_Lsize,arr_Rsize);
 	static int cnt=0;
 	int *remrgd_arr;
 	int sora=arr_Lsize+arr_Rsize;
 	remrgd_arr=(int*)malloc(sizeof(int)*sora);
+	memset(remrgd_arr,0,sora);
 	//if(arr_Lsize>=2 && arr_Rsize>=2)
 	//{
 	//printf("================START=================\n");
 	//printf("In comp_merge=%d\n",cnt);
 	printf("arr_Lsize=%d\narr_Rsize=%d\n",arr_Lsize,arr_Rsize);
+	for(i=0;i<arr_Lsize;i++)
+	printf("sorted_arr_L[%d]=%d\nsorted_arr_R[%d]=%d\n",i,i,sorted_arr_L[i],sorted_arr_R[i]);
+	printf("I AM HERE\n");
 	//}
 	//1.Compare Left and right array
        	i=0;j=0;int k=0;
 	while(i<arr_Lsize && j<arr_Rsize)
 	{
 		//Compare arr_L & arr_R and put lowest element in remrgd_arr
-		if(arr_L[i] < arr_R[j])
+		if(sorted_arr_L[i] < sorted_arr_R[j])
 		{
-			remrgd_arr[k]=arr_L[i];
+			remrgd_arr[k]=sorted_arr_L[i];
 			i++;
 		}
 		else
 		{
-			remrgd_arr[k]=arr_R[j];
+			remrgd_arr[k]=sorted_arr_R[j];
 			j++;
 		}
 		k++;
 
 	}
+	printf("1-->i=%d\tj=%d\n",i,j);
 	display_sorted_arr(remrgd_arr,sora);
 	printf("\n");
 		//Whatever is left put in remrgd array only after one of the array's are finished
@@ -120,6 +129,7 @@ int split_array(int* arr,int sizeofarr)
 		k++;
 		j++;
 	}
+	printf("2-->i=%d\tj=%d\n",i,j);
 	cnt++;
 	/*if(arr_Lsize>=2 && arr_Rsize>=2)
 	{
@@ -129,9 +139,11 @@ int split_array(int* arr,int sizeofarr)
 	//printf("================END=================\n");
 	printf("\n");
 	//}
-	free(remrgd_arr);
 	free(arr_L);
 	free(arr_R);
+	free(sorted_arr_L);
+	free(sorted_arr_R);
+	return remrgd_arr;
 }
 
 int comp_merge(int* arr_L,int* arr_R,int arr_Lsize,int arr_Rsize)
