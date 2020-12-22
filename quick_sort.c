930a734a92a9ce3b5c90139d04f79d5cda3dbf24
Abhijeet23081989 <abhijeet.srivastava2308@gmail.com>
2020-12-22 23:02:00 +0530

    Quick-Sort complete.Pivot=arr[end]

--- quick_sort.c
+++ quick_sort.c
@@ -30,23 +30,18 @@ int main()
 	start=0;end=7;
 	quick_sort(arr,start,end);
 	display(arr,end+1);
+	printf("\n");
 }
 
 int quick_sort(int* arr, int start,int end)
 {
-	int pIndex=0;static int cnt=0;
 	//First bring pivot element to its rightful place and then arrange :
         //a-->.arr[i]<pivot to left
 	//b-->arr[i]>pivot to right
 	if(start>=end)
 		return 0;
-	cnt++;
-	if(cnt==2)
-	printf("In quick sort %d\nstart=%d\nend=%d\n",cnt,start,end);
 	sleep(1);
-	pIndex=partition(arr,start,end);
-	if(cnt==2)
-	printf("pIndex in quick sort=%d\n",pIndex);
+	int pIndex=partition(arr,start,end);
 	//Then Quick sort left elements
 	quick_sort(arr,start,pIndex-1);
 	//Quick Sort right elements
@@ -56,41 +51,22 @@ int quick_sort(int* arr, int start,int end)
 //Partition is about bringing the pivot element in the middle and arranging all elements about pivot
 int partition(int * arr, int start,int end)
 {
-	int pivot,pIndex=0,i;static int cnt=0;
+	int pivot,pIndex=start,i,tmp;
 	//Taking last element as pivot element 
 	pivot=arr[end];
-	//printf("pivot=%d\n",pivot);
-	cnt++;
-	//if(cnt==2)
-	//{
-	//printf("In partion %d\n",cnt);
-	//printf("start=%d\nend=%d\n",start,end);
-	printf("pivot=%d\n",pivot);
-	//}
 	//For pivot=last element
 	for(i=start;i<end;i++)
 	{
-		//printf("arr[pIndex]=%d\ti=%d\n",arr[pIndex],i);
-		//printf("pivot=%d\n",pivot);
 		if(arr[i]<pivot)
 		{
-			if(arr[i]<arr[pIndex])
-			{
-			//swap(arr[pIndex],arr[i]);
-			arr[pIndex]=arr[pIndex]+arr[i];
-			arr[i]=arr[pIndex]-arr[i];
-			arr[pIndex]=arr[pIndex]-arr[i];
+			tmp=arr[pIndex];
+			arr[pIndex]=arr[i];
+			arr[i]=tmp;
 			pIndex=pIndex+1;	
-			}
 		}
-		if(cnt==2)
-		{
 		display(arr,8);
 		printf("\n");
-		}
 	}
-	if(cnt==2)
-	printf("pIndex=%d\n",pIndex);
 	//For pivot=first element
 	//for(i=start+1,i<=end;i++)
 	//For pivot=middle element
@@ -98,25 +74,14 @@ int partition(int * arr, int start,int end)
 	//for(mid+1-->end)
 	//For pivot=random element
 	//for(?)
-	//swap(arr[pIndex],pivot);
-	arr[pIndex]=arr[pIndex]+pivot;
-	pivot=arr[pIndex]-pivot;
-	arr[pIndex]=arr[pIndex]-pivot;
-	if(cnt==3)
-	{
-	printf("arr[pIndex]=%d\n",arr[pIndex]);
+	tmp=arr[pIndex];
+	arr[pIndex]=arr[end];
+	arr[end]=tmp;
 	display(arr,8);
 	printf("\n");
-	}
 	return pIndex;
 }
 
-void swap(int a,int b)
-{
-	a=a+b;
-	b=a-b;
-	a=a-b;
-}
 int display(int*arr,int size)
 {
 	for(int j=0;j<size;j++)
